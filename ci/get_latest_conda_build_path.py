f5aa93e76a45d7792c12c56107fbb638b5e781da
Andrew MacDonald <andrew@maccas.net>
2016-09-02 08:53:31 +1000

    Change upload process to be able to apply labels

---
+++ ci/get_latest_conda_build_path.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import yaml
+import jinja2
+import glob
+from conda_build.config import config
+from conda_build.metadata import MetaData
+from distutils.version import LooseVersion
+
+recipe_metadata = MetaData(os.path.join(sys.argv[1]))
+binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(recipe_metadata.name()))
+binary_package = sorted(glob.glob(binary_package_glob), key=LooseVersion, reverse = True)[0]
+
+print(binary_package)
