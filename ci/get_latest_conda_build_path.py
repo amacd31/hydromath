b6645c81c4e45a03297ebb5e4fb65fcef952a1f9
Andrew MacDonald <andrew@maccas.net>
2016-09-02 12:55:11 +1000

    Update get build path script for conda-build 2.0
    In conda-build 2.0 the config API changed.

--- ci/get_latest_conda_build_path.py
+++ ci/get_latest_conda_build_path.py
@@ -3,10 +3,11 @@ import os
 import yaml
 import jinja2
 import glob
-from conda_build.config import config
+from conda_build.config import Config
 from conda_build.metadata import MetaData
 from distutils.version import LooseVersion
 
+config = Config()
 recipe_metadata = MetaData(os.path.join(sys.argv[1]))
 binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(recipe_metadata.name()))
 binary_package = sorted(glob.glob(binary_package_glob), key=LooseVersion, reverse = True)[0]
