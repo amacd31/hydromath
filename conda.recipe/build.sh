bcc2eb9047b6bc7114c0ff08231199a06ca3e282
Andrew MacDonald <andrew@maccas.net>
2016-11-03 20:14:15 +1100

    Configure conda recipe to build release version
    Changed to build as cmake release build to ensure that optimizations are
    turned on (and debugging turned off).

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,6 +1,6 @@
 cp -r $RECIPE_DIR/../src $SRC_DIR
 cp -r $RECIPE_DIR/../includes $SRC_DIR
 cp -r $RECIPE_DIR/../CMakeLists.txt $SRC_DIR
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX .
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX .
 make -j${CPU_COUNT}
 make install
